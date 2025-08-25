9f4dc04c9e6111c83d83969931bc7517900b7631
Mahashweta <mahashwetamajumder056@gmail.com>
2025-08-25 16:18:39 +0530

    Add files via upload

---
+++ Day 1/subs.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float f1,f2,subtract;
+	printf("Enter the first number =");
+	scanf("%f",&f1);
+	printf("Enter the second number =");
+	scanf("%f",&f2);
+	subtract = f1-f2;
+	printf("Subtraction = %f",subtract);
+	return 0;
+}
---
+++ Day 1/sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float f1,f2,sum;
+	printf("Enter the first number =");
+	scanf("%f",&f1);
+	printf("Enter the second number =");
+	scanf("%f",&f2);
+	sum = f1+f2;
+	printf("Sum = %f",sum);
+	return 0;
+}
